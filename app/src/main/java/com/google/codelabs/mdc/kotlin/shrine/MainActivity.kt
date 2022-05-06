4cbf7ee0ffd8ce82f7757afb790522f9ea6736a2
Fernando Romero <fer_romero96@hotmail.com>
2022-05-06 10:51:14 -0500

    Se agrego un log a la funcion

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/MainActivity.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
   //Modificando
@@ -37,6 +38,6 @@ class MainActivity : AppCompatActivity(), NavigationHost {
     }
 
     fun fernandoRamaCambio(){
-
+        Log.i("Fernando", "Prueba")
     }
 }
